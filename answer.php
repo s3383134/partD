6a224c0988ea14f6c3c398a332a962da9b190649
s3383134 <s3383134@student.rmit.edu.au>
2014-09-01 06:24:53 +0000

    partC in development

--- answer.php
+++ answer.php
@@ -1,131 +1,111 @@
-<!DOCTYPE HTML PUBLIC
-"-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html401/loose.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-  <title>Answer Page</title>
-</head>
-<body bgcolor="white">
+<?php
 
-<?php 
-	
-	function showerror() {
-		die("Error " . mysql_errno() . " : " . mysql_error());
-	}
-	
-	require 'db.php';
+require_once ("MiniTemplator.class.php");
 
-	// Show all wines in a region in a <table>
-	function displayWines($connection, $query, $wineName, $wineryName, $regionName, $grapeVariety) {
-    // Run the query on the server
-		if (!($result = @ mysql_query ($query, $connection))) {
-			showerror();
-		}
+function displayWines($wineName, $wineryName, $regionName, $grapeVariety, $year){
+    // Conection: 
+    $link = mysqli_connect("localhost","webadmin","password","winestore"); 
 
-    // Find out how many rows are available
-		$rowsFound = @ mysql_num_rows($result);
-
-		// If the query has results ...
-		if ($rowsFound > 0) {
-			// ... print out a header
-			print "You searched for $wineName from $wineryName in $regionName made from $grapeVariety<br><br>";
-
-			// and start a <table>.
-			print "\n<table>\n<tr>" .
-				"\n\t<th>Wine ID</th>" .
-				"\n\t<th>Wine Name</th>" .
-				"\n\t<th>Winery Name</th>" .
-				"\n\t<th>Region Name</th>" . 
-				"\n\t<th>Grape Variety</th>" . 
-				"\n\t<th>Year</th>\n</tr>"; 
+    //consultation: 
+    $query = "SELECT 
+				w.wine_id, 
+				w.wine_name, 
+				wy.winery_name, 
+				r.region_name, 
+				w.year, 
+				gv.variety,
+				inv.cost,
+				inv.on_hand, 
+				(SELECT SUM(items.qty) FROM items WHERE items.wine_id = w.wine_id) AS total_sold,
+				(SELECT SUM(items.price) FROM items WHERE items.wine_id = w.wine_id) AS revenue
+			FROM 
+				grape_variety AS gv
+					JOIN 
+						wine_variety ON gv.variety_id = wine_variety.variety_id
+					JOIN wine AS w ON wine_variety.wine_id =  w.wine_id
+					JOIN winery AS wy ON w.winery_id = wy.winery_id
+					JOIN region AS r ON wy.region_id = r.region_id 
+					JOIN inventory AS inv ON w.wine_id = inv.wine_id"; 
 			
-			// Fetch each of the query rows
-			while ($row = @ mysql_fetch_array($result)) {
-			// Print one row of results
-			print "\n<tr>\n\t<td>{$row["wine_id"]}</td>" .
-				"\n\t<td>{$row["wine_name"]}</td>" .
-				"\n\t<td>{$row["winery_name"]}</td>" .
-				"\n\t<td>{$row["region_name"]}</td>" .
-				"\n\t<td>{$row["variety"]}</td>" .
-				"\n\t<td>{$row["year"]}</td>\n</tr>"; 
-			} //end while loop body
-			
-			//finish table 
-			print "\n</table>"; 
-		} //end if $rowsFound body 
+	// if statements to check values
+
+	if (!empty($wineName)) 
+		$query .= " AND w.wine_name LIKE \"%$wineName%\"";
+	
+	if (!empty($wineryName)) 
+		$query .= " AND wy.winery_name LIKE \"%$wineryName%\"";
+	
+	if (!empty($regionName)) 
+		$query .= " AND r.region_name = \"$region\"";
 		
-		//Report how many rows were found
-		print "<br>{$rowsFound} records found matching your criteria<br>"; 
-	} //end of function
+	if (!empty($grapeVariety)) 
+		$query .= " AND gv.variety = \"$grapeVariety\"";
+		
+	if (!empty($minYear)) 
+		$query .= " AND w.year >= \"$minYear\"";
+		
+	if (!empty($maxYear)) 
+		$query .= " AND w.year <= \"$maxYear\"";
 	
-	// Connect to the MySQL server
-	if (!($connection = @ mysql_connect(DB_HOST, DB_USER, DB_PW))) {
-		die("Could not connect");
-	}
+	if (!empty($minStock)) 
+		$query .= " AND inv.on_hand >= \"$minStock\"";
+		
+	if (!empty($maxYear)) 
+		$query .= " AND inv.on_hand <= \"$maxStock\"";
+		
+	if (!empty($minPrice)) 
+		$query .= " AND inv.cost >= \"$minPrice\"";
+		
+	if (!empty($maxPrice)) 
+		$query .= " AND inv.cost <= \"$maxPrice\"";
+		
+	// Finish query 
+	$query .= "
+		ORDER BY w.wine_name";
+
+		
+	echo $query; 
+    //execute the query. 
+    return $link->query($query);
+}
+echo $wineName; 
+function generatePage(){
+    $t = new MiniTemplator;
+
+	$t->readTemplateFromFile ("answer_template.htm");
 	
-	//get user data 
+	// GET variables
 	$wineName = $_GET['wineName']; 
 	$wineryName = $_GET['wineryName'];
 	$regionName = $_GET['regionName']; 
 	$grapeVariety = $_GET['grapeVariety']; 
+	$minYear = $_GET['minYear'];
+	$maxYear = $_GET['maxYear'];
+	$minStock = $_GET['minStock'];
+	$maxStock = $_GET['maxStock'];
+	$minPrice = $_GET['minPrice'];
+	$maxPrice = $_GET['maxPrice'];
 	
-	if (!mysql_select_db(DB_NAME, $connection)) {
-		showerror();
-	}
+	
+	// Set specified variables
+	$rows = displayWines($$wineName, $wineryName, $regionName, $grapeVariety, $year);
 		
-	//start a query 
-	$query = 
-	"SELECT DISTINCT
-		w.wine_id, 
-		w.wine_name, 
-		wy.winery_name, 
-		r.region_name, 
-		w.year, 
-		gv.variety
-	FROM 
-		wine AS w,
-		grape_variety AS gv
-			JOIN
-				wine ON gv.variety_id = wine.wine_id
-			JOIN 
-				wine_variety ON wine.wine_id = wine_variety.variety_id,
-		winery AS wy, 
-		region AS r 
-	WHERE 
-		wy.region_id = r.region_id
-	AND 
-		w.winery_id = wy.winery_id
-	"; 
-		
+	while ($row =  mysqli_fetch_array($rows)){
+	$t->setVariable('wineID',$row['wine_id']);
+	$t->setVariable('wineName', $row['wine_name']);
+	$t->setVariable('regionName', $row['region_name']);
+	$t->setVariable('grapeVariety', $row['variety']);
+	$t->setVariable('year', $row['year']);
+	$t->setVariable('cost', $row['cost']);
+	$t->setVariable('stock', $row['on_hand']);
+	$t->setVariable('totalSold', $row['total_sold']);
+	$t->setVariable('revenue', $row['revenue']);
 	
-	if (!empty($wineName)) {
-		$query .= " AND w.wine_name = '{$wineName}'";
+	$t->addBlock("wines"); 
 	}
-	
-	if (!empty($wineryName)) {
-		$query .= " AND winery_name = '{$wineryName}'";
-	}
-	
-	// If the user has specified a region, add the regionName 
-	// as an AND clause
-	if (isset($regionName) && $regionName != "All") {
-		$query .= " AND region_name = '{$regionName}'"; 
-	} 
-	
-	// If the user has specified a variety, add the grapeVariety 
-	// as an AND clause
-	if (!empty($grapeVariety)) {
-		$query .= " AND variety = '{$grapeVariety}'"; 
-	}
-	
-	//order the list 
-	$query .= " ORDER BY wine_name"; 
-	
-	//run query, show results 
-	displayWines($connection, $query, $wineName, $wineryName, $regionName, $grapeVariety); 
+	$t->generateOutput();
+
+}
+generatePage();
 	
 ?>
-
-</body>
-</html>
--- search.php
+++ search.php
@@ -1,75 +1,55 @@
-<!DOCTYPE HTML PUBLIC
-"-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html401/loose.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-  <title>Search Page</title>
-</head>
-<body bgcolor="white">
-	<?php 
-		require "db.php"; 
-		
-		// SelectDistinct() function 
-		function selectDistinct ($connection, $tableName, $attributeName, $pulldownName, $defaultValue) {
-		$defaultWithinResultSet = FALSE; 
-			
-			//  Query to find distinct values of $attributeName in $tableName 
-			$distinctQuery = "SELECT DISTINCT {$attributeName} FROM {$tableName}"; 
-			
-			//Run the distinctQuery on the databaseName
-			if (!($resultId = @ mysql_query ($distinctQuery, $connection)))
-				showerror(); 
-				
-			// Start the select widget 
-			print "\n<select name=\"{$pulldownName}\">"; 
-			
-			// Retrieve each row from the query 
-			while ($row = @ mysql_fetch_array($resultId)) {
-				
-				// Get the value for the attribute to be displayed
-				$result = $row[$attributeName]; 
-				
-				// Check if a defaultValue is set and, if so, is it the 
-				// current database value? 
-				if (isset($defaultvalue) && $result == $defaultValue) 
-					// Yes, show as selected 
-					print "\n\t<option selected value=\"{$result}\">{$result}"; 
-				else
-					// No, just show as an option 
-					print "\n\t<option value=\"{$result}\">{$result}"; 
-				print "</option>"; 
-			}
-			print "\n</select>"; 
-		} // End function 
-	?>
+<?php
 
-  <form action="answer2.php" method="GET">
-    <br>Please enter a wine name: 
-    <input type="text" name="wineName" value="">
-	<br>Please enter a winery name: 
-	<input type="text" name="wineryName" value=""> 
-	<?php 
-		// Connect to the server 
-		if (!($connection = @ mysql_connect(DB_HOST, DB_USER, DB_PW))) {
-			showerror(); 
-		}
+require_once ("MiniTemplator.class.php");
+
+function getSearch($tableName, $attributeName) {
+
+    //conection: 
+    $link = mysqli_connect("localhost","webadmin","password","winestore") or die("Error " . mysqli_error($link)); 
+
+    //consultation: 
+    $query = "SELECT DISTINCT {$attributeName} FROM {$tableName} ORDER BY {$attributeName}";
+
+    //execute the query. 
+    return $link->query($query);
+	
+}
+
+function generatePage(){
+    $t = new MiniTemplator;
+
+    $t->readTemplateFromFile ("search_template.htm");
+
+    $rows = getSearch("region", "region_name");
+	while ($row =  mysqli_fetch_array($rows)){
+		$t->setVariable('regionName', $row['region_name']);
 		
-		if (!mysql_select_db(DB_NAME, $connection)) {
-			showerror(); 
-		}
+		$t->addBlock("regionName");
+    }
+    
+	$rows = getSearch("grape_variety", "variety");
+	while ($row =  mysqli_fetch_array($rows)){
+		$t->setVariable('variety', $row['variety']);
 		
-		print "<br>\nRegion: ";
+		$t->addBlock("variety");
+    }
+	
+	$rows = getSearch("wine", "year");
+	while ($row =  mysqli_fetch_array($rows)){
+		$t->setVariable('minYear', $row['year']);
 		
-		selectDistinct($connection, "region", "region_name", "regionName", "All"); 
+		$t->addBlock("minYear");
+    }
+	
+	$rows = getSearch("wine", "year");
+	while ($row =  mysqli_fetch_array($rows)){
+		$t->setVariable('maxYear', $row['year']);
 		
-		print "<br>\nGrape Variety: "; 
-		
-		selectDistinct($connection, "grape_variety", "variety", "grapeVariety"); 
-		
-	?>
-    <br><br><input type="submit" value="Search">
-  </form>
-  <br>
-</body>
-</html>
+		$t->addBlock("maxYear");
+    }
+	$t->generateOutput();
+}
+
+generatePage();
+
+?>

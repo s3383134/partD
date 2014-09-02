19d8a92bf6212ce310716aafbd0419b5dc5058e0
s3383134 <s3383134@student.rmit.edu.au>
2014-09-02 12:32:35 +0000

    finished answer.php, final commit

--- answer.php
+++ answer.php
@@ -1,70 +1,75 @@
 <?php
 
 require_once ("MiniTemplator.class.php");
+require_once ("db_pdo.php");
 
 function displayWines($wineName, $wineryName, $regionName, $grapeVariety, $year){
-    // Conection: 
-    $link = mysqli_connect("localhost","webadmin","password","winestore"); 
+	global $pdo; 
 
-    //consultation: 
-    $query = "SELECT 
-				w.wine_id, 
-				w.wine_name, 
-				wy.winery_name, 
-				r.region_name, 
-				w.year, 
-				gv.variety,
-				inv.cost,
-				inv.on_hand, 
-				(SELECT SUM(it.qty) FROM items AS it WHERE it.wine_id = w.wine_id) AS total_sold,
-				(SELECT SUM(it.price) FROM items AS it WHERE it.wine_id = w.wine_id) AS wine_revenue
-			FROM 
-				grape_variety AS gv
-					JOIN 
-						wine_variety ON gv.variety_id = wine_variety.variety_id
-					JOIN wine AS w ON wine_variety.wine_id =  w.wine_id
-					JOIN winery AS wy ON w.winery_id = wy.winery_id
-					JOIN region AS r ON wy.region_id = r.region_id 
-					JOIN inventory AS inv ON w.wine_id = inv.wine_id
-		"; 
+	try {
+		//consultation: 
+		$query = "SELECT 
+					w.wine_id, 
+					w.wine_name, 
+					wy.winery_name, 
+					r.region_name, 
+					w.year, 
+					gv.variety,
+					inv.cost,
+					inv.on_hand, 
+					(SELECT SUM(it.qty) FROM items AS it WHERE it.wine_id = w.wine_id) AS total_sold,
+					(SELECT SUM(it.price) FROM items AS it WHERE it.wine_id = w.wine_id) AS wine_revenue
+				FROM 
+					grape_variety AS gv
+						JOIN 
+							wine_variety ON gv.variety_id = wine_variety.variety_id
+						JOIN wine AS w ON wine_variety.wine_id =  w.wine_id
+						JOIN winery AS wy ON w.winery_id = wy.winery_id
+						JOIN region AS r ON wy.region_id = r.region_id 
+						JOIN inventory AS inv ON w.wine_id = inv.wine_id
+			"; 
+				
+		// if statements to check values
+
+		if (!empty($wineName)) 
+			$query .= " AND w.wine_name = \"$wineName\""; 
+		
+		if (!empty($wineryName)) 
+			$query .= " AND wy.winery_name = \"$wineryName\"";
+		
+		if (!empty($regionName)) 
+			$query .= " AND r.region_name = \"$regionName\"";
 			
-	// if statements to check values
-
-	if (!empty($wineName)) 
-		$query .= " AND w.wine_name = \"$wineName\""; 
+		if (!empty($grapeVariety)) 
+			$query .= " AND gv.variety = \"$grapeVariety\"";
+			
+		if (!empty($minYear)) 
+			$query .= " AND w.year >= \"$minYear\"";
+			
+		if (!empty($maxYear)) 
+			$query .= " AND w.year <= \"$maxYear\"";
+		
+		if (!empty($minStock)) 
+			$query .= " AND inv.on_hand >= \"$minStock\"";
+			
+		if (!empty($maxYear)) 
+			$query .= " AND inv.on_hand <= \"$maxStock\"";
+			
+		if (!empty($minPrice)) 
+			$query .= " AND inv.cost >= \"$minPrice\"";
+			
+		if (!empty($maxPrice)) 
+			$query .= " AND inv.cost <= \"$maxPrice\"";
+			
+		// Finish query 
+		$query .= "ORDER BY w.wine_name";
 	
-	if (!empty($wineryName)) 
-		$query .= " AND wy.winery_name = \"$wineryName\"";
-	
-	if (!empty($regionName)) 
-		$query .= " AND r.region_name = \"$regionName\"";
-		
-	if (!empty($grapeVariety)) 
-		$query .= " AND gv.variety = \"$grapeVariety\"";
-		
-	if (!empty($minYear)) 
-		$query .= " AND w.year >= \"$minYear\"";
-		
-	if (!empty($maxYear)) 
-		$query .= " AND w.year <= \"$maxYear\"";
-	
-	if (!empty($minStock)) 
-		$query .= " AND inv.on_hand >= \"$minStock\"";
-		
-	if (!empty($maxYear)) 
-		$query .= " AND inv.on_hand <= \"$maxStock\"";
-		
-	if (!empty($minPrice)) 
-		$query .= " AND inv.cost >= \"$minPrice\"";
-		
-	if (!empty($maxPrice)) 
-		$query .= " AND inv.cost <= \"$maxPrice\"";
-		
-	// Finish query 
-	$query .= "ORDER BY w.wine_name";
-	
-    //execute the query. 
-    return $link->query($query);
+		//execute the query. 
+		return $pdo->query($query);
+	} catch (PDOException $e) {
+		echo $e->getMessage(); 
+		exit; 
+	}
 }
 
 
@@ -90,9 +95,9 @@ function generatePage(){
 	$maxPrice = $_GET['maxPrice'];	
 
 	// Set specified variables
-	$rows = displayWines($wineName, $wineryName, $regionName, $grapeVariety, $year);
+	$result = displayWines($wineName, $wineryName, $regionName, $grapeVariety, $year);
 		
-	while ($row =  mysqli_fetch_array($rows)){
+	while ($row =  $result->fetch(PDO::FETCH_ASSOC)){
 		$t->setVariable('wineID',$row['wine_id']);
 		$t->setVariable('wineName', $row['wine_name']);
 		$t->setVariable('regionName', $row['region_name']);
